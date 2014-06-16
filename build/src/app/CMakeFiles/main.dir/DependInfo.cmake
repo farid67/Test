759a4a71a20adeeed65bb24d1bd8ffffaf6a8af8
farid <farid@farid-VirtualBox.(none)>
2014-06-16 20:21:30 +0200

    ajout cibles

---
+++ build/src/app/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/var/lib/jenkins/workspace/Test-jenkins_03.06/src/app/main.c" "/var/lib/jenkins/workspace/Test-jenkins_03.06/build/src/app/CMakeFiles/main.dir/main.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/var/lib/jenkins/workspace/Test-jenkins_03.06/build/src/lib/CMakeFiles/libTest.dir/DependInfo.cmake"
+  )
---
+++ build/src/lib/CMakeFiles/libTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/libTest.dir/test.c.o"
+  "liblibTest.pdb"
+  "liblibTest.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/libTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
