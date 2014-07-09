1a3da1b2d0b2b90c3a2d2823e8bc73c05e53ef4d
root <root@farid-VirtualBox.(none)>
2014-07-09 15:19:39 +0200

    utilisation du nom du noeud comme intitulé du projet dans cdash -> modification des scripts

--- create_CTestConfig.bat
+++ create_CTestConfig.bat
@@ -1,9 +1,8 @@
 @echo off
-echo job name : %JOB_NAME%
-echo "set(CTEST_PROJECT_NAME \"%project_name%\")" >> CTestConfig.cmake
+echo "set(CTEST_PROJECT_NAME \"%NODE_NAME%\")" >> CTestConfig.cmake
 echo "set(CTEST_NIGHTLY_START_TIME \"01:00:00 UTC\")" >> CTestConfig.cmake
 echo " " >> CTestConfig.cmake
 echo " set(CTEST_DROP_METHOD \"http\") " >> CTestConfig.cmake
 echo " set(CTEST_DROP_SITE \"192.168.56.2\")" >> CTestConfig.cmake
-echo " set(CTEST_DROP_LOCATION \"/CDash/submit.php?project=%project_name%\")" >> CTestConfig.cmake
+echo " set(CTEST_DROP_LOCATION \"/CDash/submit.php?project=%JOB_NAME%\")" >> CTestConfig.cmake
 echo " set(CTEST_DROP_SITE_CDASH TRUE)" >> CTestConfig.cmake
